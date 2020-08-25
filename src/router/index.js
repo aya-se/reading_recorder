cd05a39a0ef7a8ac3c1fc5c9df4c1e1824dbd44c
aya-se <kakkeno1@gmail.com>
2020-08-25 11:11:06 +0900

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import BookSearch from "../views/BookSearch.vue";
+import BookForm from "../views/BookForm.vue";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
@@ -11,13 +13,18 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/search",
+    name: "search",
+    component: BookSearch
+  },
+  {
+    path: "/form",
+    name: "form",
+    component: BookForm
+  },
+  {
+    path: "*",
+    redirect: "/"
   }
 ];
 
